c213b2d20b80dadb8a64aaa250b50b92044968ba
samuelmoiwa <samuelmoiwa@gmail.com>
2023-04-18 11:14:26 +0000

    update package.json

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -1,10 +1,92 @@
-import React from "react";
+import React from 'react';
+// import { useForm } from 'react-hook-form';
+// import { HiOutlineMail } from 'react-icons/hi';
+// import { IoMdCall } from 'react-icons/io';
+// import { AiOutlineLocation } from 'react-icons/ai';
+// import 'tailwindcss/tailwind.css';
 
 const ContactUs = () => {
+
+  // const { register, handleSubmit, errors } = useForm();
+
+  // const onSubmit = (data) => {
+  //   console.log(data);
+  // };
+
   return
-  <div>
-    <h1>Contact Us</h1>
-  </div>;
+//   <div className="bg-gray-100 py-10">
+//   <div className="container mx-auto">
+//     <h1 className="text-3xl font-bold mb-4">Contact Us</h1>
+//     <div className="flex flex-col md:flex-row">
+//       <div className="w-full md:w-1/2 md:pr-8 mb-8 md:mb-0">
+//         <form onSubmit={handleSubmit(onSubmit)}>
+//           <div className="mb-4">
+//             <label className="block text-gray-700 font-bold mb-2" htmlFor="name">
+//               Name
+//             </label>
+//             <input
+//               className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+//               id="name"
+//               name="name"
+//               type="text"
+//               placeholder="Enter your name"
+//               ref={register({ required: true })}
+//             />
+//             {errors.name && <span className="text-red-500 text-sm">This field is required</span>}
+//           </div>
+//           <div className="mb-4">
+//             <label className="block text-gray-700 font-bold mb-2" htmlFor="email">
+//               Email
+//             </label>
+//             <input
+//               className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+//               id="email"
+//               name="email"
+//               type="email"
+//               placeholder="Enter your email"
+//               ref={register({ required: true })}
+//             />
+//             {errors.email && <span className="text-red-500 text-sm">This field is required</span>}
+//           </div>
+//           <div className="mb-4">
+//             <label className="block text-gray-700 font-bold mb-2" htmlFor="message">
+//               Message
+//             </label>
+//             <textarea
+//               className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+//               id="message"
+//               name="message"
+//               placeholder="Enter your message"
+//               rows="5"
+//               ref={register({ required: true })}
+//             />
+//             {errors.message && <span className="text-red-500 text-sm">This field is required</span>}
+//           </div>
+//           <button
+//             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+//             type="submit"
+//           >
+//             Submit
+//           </button>
+//         </form>
+//       </div>
+//       <div className="w-full md:w-1/2">
+//         <div className="mb-4 flex items-center">
+//           <HiOutlineMail className="text-2xl mr-2" />
+//           <span>contact    @example.com</span>
+//         </div>
+//         <div className="mb-4 flex items-center">
+//           <IoMdCall className="text-2xl mr-2" />
+//           <span>+1 234 567 8901</span>
+//         </div>
+//         <div className="flex items-center">
+//           <AiOutlineLocation className="text-2xl mr-2" />
+//           <span>123 Main St, New York, NY 10001, USA</span>
+//         </div>
+//       </div>
+//     </div>
+//   </div>
+// </div>
 };
 
 export default ContactUs;
